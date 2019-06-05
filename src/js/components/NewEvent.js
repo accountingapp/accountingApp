134318b2d4ea18a5f946ea5d42168c74451d6cf1
Marissa Pels <marissapels@gmail..com>
2019-06-04 20:52:46 -0500

    Add prettier

--- src/js/components/NewEvent.js
+++ src/js/components/NewEvent.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Image from 'react-bootstrap/Image';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import FormControl from 'react-bootstrap/FormControl';
-import ListGroup from 'react-bootstrap/ListGroup';
-import {Link} from 'react-router-dom';
-import axios from 'axios';
+import React, { Component } from "react";
+import InputGroup from "react-bootstrap/InputGroup";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import FormControl from "react-bootstrap/FormControl";
+import ListGroup from "react-bootstrap/ListGroup";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 class NewEvent extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
       title: "",
-      description: ""
-    }
+      description: "",
+      stages: []
+    };
   }
 
   handleChange(e) {
@@ -26,55 +26,86 @@ class NewEvent extends Component {
     });
   }
 
+  addStage() {
+    let currentStages = this.state.stages;
+    let stageObject = {
+      stage: "",
+      description: "",
+      financialImpact: "",
+      accounts: []
+    };
+    currentStages.push(stageObject);
+
+    this.setState({
+      stages: currentStages
+    });
+  }
+
+  addAccount(stageNumber) {
+    let currentStages = this.state.stages;
+    let accountObject = {
+      accountDescription: "",
+      debitCredit: "",
+      amount: "",
+      currency: "",
+      accountType: "",
+      increaseDecrease: ""
+    };
+
+    currentStages[stageNumber].accounts.push(accountObject);
+
+    this.setState({
+      stages: currentStages
+    });
+  }
+
   render() {
-    return(
+    return (
       <div>
-          <Row>
-            <Col md={8} className="mainNewEventPanel">
-
+        <Row>
+          <Col md={8} className="mainNewEventPanel">
             <div className="eventHeader">
-            <h3>Create a new Event</h3>
+              <h3>Create a new Event</h3>
               <div className="formGroup">
-              <label>Title</label>
+                <label>Title</label>
                 <input
-                  id='title'
+                  id="title"
                   value={this.state.title}
-                  onChange={(e)=>this.handleChange(e)}
+                  onChange={e => this.handleChange(e)}
                   className="inputField"
                 />
-                </div>
+              </div>
 
-            <div className="formGroup">
-              <label>Description</label>
+              <div className="formGroup">
+                <label>Description</label>
                 <input
-                  id='description'
+                  id="description"
                   value={this.state.description}
-                  onChange={(e)=>this.handleChange(e)}
+                  onChange={e => this.handleChange(e)}
                   className="inputField"
                 />
-                </div>
-                </div>
+              </div>
+            </div>
 
             <div className="eventHeader">
-            <h3>Stage 1</h3>
+              <h3>Stage 1</h3>
               <div className="formGroup">
-              <label>Account</label>
+                <label>Account</label>
                 <input
-                  id='account'
+                  id="account"
                   value={this.state.account}
-                  onChange={(e)=>this.handleChange(e)}
+                  onChange={e => this.handleChange(e)}
                   className="inputField"
                 />
-                </div>
-                </div>
-
-            </Col>
-            <Col md={4} className="sideNewEventPanel">
-              <h3>Additional Panel Info</h3>
-            </Col>
-          </Row>
+              </div>
+            </div>
+          </Col>
+          <Col md={4} className="sideNewEventPanel">
+            <h3>Additional Panel Info</h3>
+          </Col>
+        </Row>
       </div>
-    )
+    );
   }
 }
 
