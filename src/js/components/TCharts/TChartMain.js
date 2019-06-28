1a4a44bed02c0753f8c901847e8d2ae33d68a95d
Marissa Pels <marissapels@gmail..com>
2019-06-27 21:44:24 -0500

    t chart page

---
+++ src/js/components/TCharts/TChartMain.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Table } from "react-bootstrap";
+import TChart from "./TChart";
+
+class TChartMain extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      credits: [],
+      debits: []
+    };
+  }
+
+  render() {
+    const { props } = this.props;
+    return (
+      <div>
+        <h1>T-Charts</h1>
+        <Container>
+          <Row>
+            <Col md={4}>
+              <TChart />
+            </Col>
+            <Col md={4}>
+              <TChart />
+            </Col>
+            <Col md={4}>
+              <TChart />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default TChartMain;
